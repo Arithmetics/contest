5871949448b862cde60cf7e0f6c763ee66b0616f
Arithmetics <brock.m.tillotson@gmail.com>
2021-03-14 15:38:45 -0700

    adding for deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,13 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
-  useColorModeValue,
   useBreakpointValue,
   useDisclosure,
+  Heading,
+  Avatar,
+  Center,
+  Image,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon, ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
@@ -78,6 +82,7 @@ export default function HomePage(): JSX.Element {
       <Collapse in={isOpen} animateOpacity>
         <MobileNav />
       </Collapse>
+      <SocialProfileWithImage />
     </Box>
   );
 }
@@ -271,3 +276,75 @@ const NAV_ITEMS: Array<NavItem> = [
     href: '#',
   },
 ];
+
+function SocialProfileWithImage(): JSX.Element {
+  return (
+    <Center py={6}>
+      <Box
+        maxW={'270px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.600')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        overflow={'hidden'}
+      >
+        <Image
+          h={'120px'}
+          w={'full'}
+          src={
+            'https://images.squarespace-cdn.com/content/v1/5f6a2443c6004d000a8d74df/1611684154674-DDO2M3A64SNPGOTBQWE6/ke17ZwdGBToddI8pDm48kNLmoMgP9-PoGL3pTpvfmf97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UZn9JnVc0xDeoNGVJ3wrjE4Nx_EhhKi4CB_8Hn-bafWShA1iIgJHGOspu562nPK3kQ/11+Sonics.jpg?format=2500w'
+          }
+          objectFit={'cover'}
+        />
+        <Flex justify={'center'} mt={-12}>
+          <Avatar
+            size={'xl'}
+            src={'https://exnba.com/wp-content/uploads/2013/09/payton-looks-on.jpg'}
+            alt={'Author'}
+            css={{
+              border: '2px solid white',
+            }}
+          />
+        </Flex>
+
+        <Box p={6}>
+          <Stack spacing={0} align={'center'} mb={5}>
+            <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+              Sonics
+            </Heading>
+            <Text color={'gray.500'}>Frontend Developer</Text>
+          </Stack>
+
+          <Stack direction={'row'} justify={'center'} spacing={6}>
+            <Stack spacing={0} align={'center'}>
+              <Text fontWeight={600}>23k</Text>
+              <Text fontSize={'sm'} color={'gray.500'}>
+                Bets
+              </Text>
+            </Stack>
+            <Stack spacing={0} align={'center'}>
+              <Text fontWeight={600}>23k</Text>
+              <Text fontSize={'sm'} color={'gray.500'}>
+                Other bets
+              </Text>
+            </Stack>
+          </Stack>
+
+          <Button
+            w={'full'}
+            mt={8}
+            bg={useColorModeValue('#151f21', 'gray.900')}
+            color={'white'}
+            rounded={'md'}
+            _hover={{
+              transform: 'translateY(-2px)',
+              boxShadow: 'lg',
+            }}
+          >
+            Bet
+          </Button>
+        </Box>
+      </Box>
+    </Center>
+  );
+}
