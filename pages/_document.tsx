e3fdad60cfee7b4da6c38b0a621796934052d2fe
Arithmetics <brock.m.tillotson@gmail.com>
2021-03-14 15:42:51 -0700

    moved theme out of page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
-import theme from './theme';
+import theme from '../theme';
 
 export default class MyDocument extends Document {
   render() {
