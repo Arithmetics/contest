9f1a85b7aa6f69efb59c9e964149e42f7ff67a07
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-04-02 20:32:41 -0700

    added nprogress, cool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import NextApp from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+import '../components/nprogress.css';
+
 import Page from '../components/Page';
 import theme from '../theme';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default class App extends NextApp {
   render(): JSX.Element {
