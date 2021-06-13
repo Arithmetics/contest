e74308a5a1d3d7ec10702ec7fdbd0ba5ff4bc58a
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-06-13 14:39:01 -0700

    nav

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,119 +1,178 @@
-import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
 import {
-  Button,
-  Collapse,
+  chakra,
+  Box,
   Flex,
-  IconButton,
-  Stack,
-  Text,
-  useBreakpointValue,
+  useColorModeValue,
+  HStack,
   useDisclosure,
+  VStack,
+  IconButton,
+  CloseButton,
+  ThemingProps,
 } from '@chakra-ui/react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { IoIosBasketball } from 'react-icons/io';
+import { GiAmericanFootballHelmet, GiAmericanFootballBall } from 'react-icons/gi';
+import { useRouter } from 'next/router';
 
-import DesktopNav from './DesktopNav';
-import MobileNav from './MobileNav';
+import { Routes } from '../../constants';
+import LogOut from '../auth/LogOut';
 
-export interface NavItem {
-  label: string;
-  subLabel?: string;
-  children?: Array<NavItem>;
-  href?: string;
-}
-
-export const NAV_ITEMS: NavItem[] = [
-  {
-    label: 'Contests',
-    children: [
-      {
-        label: 'NFL Over Under 2021',
-        subLabel: 'Open',
-        href: '#',
-      },
-      {
-        label: 'NBA Over Under 2021',
-        subLabel: 'Coming Soon!',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Find Work',
-    children: [
-      {
-        label: 'Job Board',
-        subLabel: 'Find your dream design job',
-        href: '#',
-      },
-      {
-        label: 'Freelance Projects',
-        subLabel: 'An exclusive list for contract work',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Profile',
-    href: '#',
-  },
-];
+import ButtonLink from '../ButtonLink';
+import ChakraLink from '../ChakraLink';
+import { useUser } from '../User';
+import AvatarLink from '../AvatarLink';
 
 export default function Nav(): JSX.Element {
-  const { isOpen, onToggle } = useDisclosure();
+  const mobileNav = useDisclosure();
+  const router = useRouter();
+
+  const user = useUser();
+
+  const avatarUrl = user?.avatarImage?.image?.publicUrlTransformed;
+
+  const determineLinkStyle = (link: string): ThemingProps<'Button'> => {
+    if (router.pathname === link) {
+      return { variant: 'solid', size: 'sm', colorScheme: 'teal' };
+    }
+    return { variant: 'ghost', size: 'sm', colorScheme: 'white' };
+  };
 
   return (
     <>
-      <Flex
-        bg={'gray.900'}
-        color={'white'}
-        minH={'60px'}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={'solid'}
-        borderColor={'gray.900'}
-        align={'center'}
-        position={'sticky'}
-        top={0}
-      >
-        <Flex
-          flex={{ base: 1, md: 'auto' }}
-          ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'none' }}
-        >
-          <IconButton
-            onClick={onToggle}
-            icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
-          />
+      <chakra.header bg="gray.900" w="full" px={{ base: 2, sm: 4 }} py={4} shadow="md">
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <HStack display="flex" spacing={3} alignItems="center">
+            <Box display={{ base: 'inline-flex', md: 'none' }}>
+              <IconButton
+                display={{ base: 'flex', md: 'none' }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color={useColorModeValue('gray.800', 'inherit')}
+                variant="ghost"
+                icon={<AiOutlineMenu />}
+                onClick={mobileNav.onOpen}
+              />
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? 'flex' : 'none'}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg="gray.900"
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
+              >
+                <CloseButton
+                  aria-label="Close menu"
+                  justifySelf="self-start"
+                  onClick={mobileNav.onClose}
+                />
+                <ButtonLink
+                  title="NFL ATS"
+                  leftIcon={<GiAmericanFootballBall />}
+                  href={`/${Routes.CONTESTS}/nflPlayoffATS`}
+                  buttonTheme={{ variant: 'ghost' }}
+                  layoutProps={{ w: 'full' }}
+                />
+                <ButtonLink
+                  title="NFL Over Under"
+                  leftIcon={<GiAmericanFootballHelmet />}
+                  href={`/${Routes.CONTESTS}/nflOverUnder`}
+                  buttonTheme={{ variant: 'ghost' }}
+                  layoutProps={{ w: 'full' }}
+                />
+                <ButtonLink
+                  title="NBA Over Under"
+                  leftIcon={<IoIosBasketball />}
+                  href={`/${Routes.CONTESTS}/nbaOverUnder`}
+                  buttonTheme={{ variant: 'ghost' }}
+                  layoutProps={{ w: 'full' }}
+                />
+              </VStack>
+            </Box>
+            {/* <chakra.a href="/" title="Home page" display="flex" alignItems="center">
+              Logo
+            </chakra.a> */}
+
+            <ChakraLink href="/" title="LOGO" />
+
+            <HStack spacing={3} display={{ base: 'none', md: 'inline-flex' }}>
+              <ButtonLink
+                title="NFL ATS"
+                leftIcon={<GiAmericanFootballBall />}
+                href={`/${Routes.CONTESTS}/nflPlayoffATS`}
+                buttonTheme={determineLinkStyle(`/${Routes.CONTESTS}/nflPlayoffATS`)}
+              />
+              <ButtonLink
+                title="NFL Over Under"
+                leftIcon={<GiAmericanFootballHelmet />}
+                href={`/${Routes.CONTESTS}/nflOverUnder`}
+                buttonTheme={determineLinkStyle(`/${Routes.CONTESTS}/nflOverUnder`)}
+              />
+              <ButtonLink
+                title="NBA Over Under"
+                leftIcon={<IoIosBasketball />}
+                href={`/${Routes.CONTESTS}/nbaOverUnder`}
+                buttonTheme={determineLinkStyle(`/${Routes.CONTESTS}/nbaOverUnder`)}
+              />
+            </HStack>
+          </HStack>
+          <HStack spacing={3} display={mobileNav.isOpen ? 'none' : 'flex'} alignItems="center">
+            {user ? (
+              <>
+                <AvatarLink
+                  size="md"
+                  name={user.userName || ''}
+                  src={avatarUrl || ''}
+                  title="account"
+                  href="/account"
+                />
+                <LogOut />
+              </>
+            ) : (
+              <>
+                <ButtonLink
+                  title="Sign Up"
+                  href={`/${Routes.SIGNUP}`}
+                  buttonTheme={{ variant: 'ghost', colorScheme: 'teal' }}
+                />
+                <ButtonLink
+                  title="Log In"
+                  href={`/${Routes.LOGIN}`}
+                  buttonTheme={{ variant: 'ghost', colorScheme: 'teal' }}
+                />
+              </>
+            )}
+          </HStack>
         </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: 'start', md: 'start' }}>
-          <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily={'heading'}
-            color={'white'}
-          >
-            Logo
-          </Text>
-
-          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-            <DesktopNav />
-          </Flex>
-        </Flex>
-
-        <Stack flex={{ base: 1, md: 0 }} justify={'flex-end'} direction={'row'} spacing={6}>
-          <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'login'}>
-            Sign In
-          </Button>
-          <Button as={'a'} fontSize={'sm'} fontWeight={600} color={'white'} href={'signup'}>
-            Sign Up
-          </Button>
-        </Stack>
-      </Flex>
-
-      <Collapse in={isOpen} animateOpacity>
-        <MobileNav />
-      </Collapse>
+      </chakra.header>
+      {/* <Flex alignItems="center" justifyContent="center" mx={2} borderWidth={0} overflowX="auto">
+        <Tabs defaultIndex={1} borderBottomColor="transparent" colorScheme="teal">
+          <TabList>
+            <Tab py={4} m={0} _focus={{ boxShadow: 'none' }}>
+              Make Picks
+            </Tab>
+            <Tab py={4} m={0} _focus={{ boxShadow: 'none' }}>
+              Leaderboard
+            </Tab>
+            <Tab py={4} m={0} _focus={{ boxShadow: 'none' }}>
+              Rules
+            </Tab>
+            <Tab py={4} m={0} _focus={{ boxShadow: 'none' }}>
+              Tracker
+            </Tab>
+            <Tab py={4} m={0} _focus={{ boxShadow: 'none' }}>
+              History
+            </Tab>
+          </TabList>
+        </Tabs>
+      </Flex> */}
     </>
   );
 }
