07e87b697aa47247b155f8e0213fe921aeba6db8
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-06-25 11:45:52 -0700

    coming soons

--- pages/contests/nflPlayoffATS.tsx
+++ pages/contests/nflPlayoffATS.tsx
@@ -1,10 +1,9 @@
-import ContestNav from '../../components/nav/ContestNav';
+import { Center, Text } from '@chakra-ui/react';
 
 export default function NFLPlayoffATS(): JSX.Element {
   return (
-    <>
-      <ContestNav />
-      <p>NFL Playoff ATS Page</p>
-    </>
+    <Center marginTop={'30vh'}>
+      <Text fontSize="2xl">Coming soon!</Text>
+    </Center>
   );
 }
