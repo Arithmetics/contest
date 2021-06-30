142286537efbb9501dc250935e4247e0996a11f9
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-06-30 14:32:40 -0700

    seeding standings and graphs somewhat up and running

--- components/contests/TrackerTab.tsx
+++ components/contests/TrackerTab.tsx
@@ -8,7 +8,9 @@ import {
   Divider,
   StatLabel,
   StatNumber,
+  VStack,
 } from '@chakra-ui/react';
+import { ResponsiveLine, Serie } from '@nivo/line';
 import { Contest, Line, useTrackerStatusQuery } from '../../generated/graphql-types';
 
 type TrackerTabProps = {
@@ -28,9 +30,11 @@ export default function TrackerTab({ contest }: TrackerTabProps): JSX.Element {
   console.log(data);
   return (
     <Center>
-      {data?.allLines?.map((line) => {
-        return <TrackerGraphCard key={line?.id} line={line as Line} />;
-      })}
+      <VStack>
+        {data?.allLines?.map((line) => {
+          return <TrackerGraphCard key={line?.id} line={line as Line} />;
+        })}
+      </VStack>
     </Center>
   );
 }
@@ -66,6 +70,95 @@ function TrackerGraphCard({ line }: TrackerGraphCardProps): JSX.Element {
         />
       </HStack>
       <Divider orientation="horizontal" paddingTop={3} />
+      <Box height={'300px'} width={'100%'}>
+        <MyResponsiveLine data={prepareLineStandingsForGraph(line as Line)} />
+      </Box>
     </Box>
   );
+}
+
+type XX = {
+  data: Serie[];
+};
+
+function MyResponsiveLine({ data }: XX): JSX.Element {
+  console.log(data);
+  return (
+    <ResponsiveLine
+      data={data}
+      theme={{
+        fontSize: 1,
+        textColor: '#fff',
+      }}
+      margin={{ top: 50, right: 50, bottom: 60, left: 60 }}
+      xScale={{ type: 'linear', min: 0, max: 17 }}
+      yScale={{ type: 'linear', min: 0, max: 17, reverse: false }}
+      // yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 1,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'week',
+        legendOffset: 36,
+        legendPosition: 'middle',
+      }}
+      axisLeft={{
+        tickSize: 1,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'win %',
+        legendOffset: -40,
+        legendPosition: 'middle',
+      }}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      // legends={[
+      //   {
+      //     anchor: 'bottom-right',
+      //     direction: 'column',
+      //     justify: false,
+      //     translateX: 100,
+      //     translateY: 0,
+      //     itemsSpacing: 0,
+      //     itemDirection: 'left-to-right',
+      //     itemWidth: 80,
+      //     itemHeight: 20,
+      //     itemOpacity: 0.75,
+      //     symbolSize: 12,
+      //     symbolShape: 'circle',
+      //     symbolBorderColor: 'rgba(0, 0, 0, .5)',
+      //     effects: [],
+      //   },
+      // ]}
+    />
+  );
+}
+
+function prepareLineStandingsForGraph(line: Line): Serie[] {
+  const standings = line?.standings;
+  if (!standings || standings.length === 0) {
+    return [];
+  }
+  const serieResults: Serie = {
+    id: 'standing',
+    data: [],
+  };
+
+  const serieBenchmark: Serie = {
+    id: 'benchmark',
+    data: [],
+  };
+
+  line.standings.forEach((standing) => {
+    serieResults.data.push({ x: standing.gamesPlayed, y: standing.wins });
+    serieBenchmark.data.push({ x: standing.gamesPlayed, y: line.benchmark });
+  });
+
+  return [serieResults, serieBenchmark];
 }
