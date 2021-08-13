e9dada595c3f60078df5b9992279d6d9489e42f0
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-08-13 16:08:05 -0700

    new images

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -8,13 +8,13 @@ export default function SignUpPage(): JSX.Element {
       as={SimpleGrid}
       maxW={'7xl'}
       columns={{ base: 1, md: 2 }}
-      spacing={{ base: 10, lg: 32 }}
-      py={{ base: 10, sm: 20, lg: 32 }}
+      spacing={{ base: 0 }}
+      py={{ base: 10, sm: 20, lg: 20 }}
     >
       {show && (
         <Flex flex={1}>
           <Image
-            alt={'Login Image'}
+            alt={'Signup Image'}
             objectFit={'cover'}
             src={'https://cdn131.picsart.com/325450196018211.png?type=webp&to=min&r=640'}
           />
