db718e3b22895e8d643bc2d1d522e59fc8168adb
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-11-08 09:28:10 -0800

    redeploy

--- backend/admin/config.tsx
+++ backend/admin/config.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { AdminConfig } from '@keystone-next/types';
-
+//
 // @ts-ignore
 const CustomLogo = () => <h2>BT Bets</h2>;
 
