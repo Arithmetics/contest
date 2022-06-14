c1184a56e566d965f6999ad3478ea8c228b0f285
Brock Tillotson <brock.m.tillotson@gmail.com>
2022-06-13 20:44:41 -0700

    histories working

--- components/contests/HistoryTab.tsx
+++ components/contests/HistoryTab.tsx
@@ -1,65 +1,30 @@
-import {
-  Center,
-  Spinner,
-  Text,
-  Box,
-  Table,
-  Thead,
-  Tr,
-  Th,
-  Tbody,
-  Td,
-  useBreakpointValue,
-  HStack,
-  Avatar,
-} from '@chakra-ui/react';
-import { useHistoriesByTypeQuery, HistoryContestTypeType } from '../../generated/graphql-types';
+import { Center, Spinner } from '@chakra-ui/react';
+import { useContestByIdQuery, HistoryContestTypeType } from '../../generated/graphql-types';
+import HistoryTable from './HistoryTable';
 
-export default function HistoryTab(): JSX.Element {
-  const { data, loading } = useHistoriesByTypeQuery({
-    variables: { contestType: HistoryContestTypeType.NflAts },
+type HistoryTabProps = {
+  contestId?: string;
+};
+
+export default function HistoryTab({ contestId }: HistoryTabProps): JSX.Element {
+  const { data: contestData, loading: getContestLoading } = useContestByIdQuery({
+    variables: {
+      id: contestId || '',
+    },
   });
 
-  const marginBox = useBreakpointValue({ base: 1, sm: 2, md: 6 });
+  const contestType = contestData?.contest?.contestType;
 
-  if (loading) {
+  if (getContestLoading) {
     return (
       <Center marginTop={'30vh'}>
         <Spinner color="red.500" marginLeft="auto" marginRight="auto" size="xl" />
       </Center>
     );
   }
+  if (contestType) {
+    return <HistoryTable contestType={(contestType as unknown) as HistoryContestTypeType} />;
+  }
 
-  return (
-    <Box borderWidth="1px" borderRadius="lg" padding={marginBox} m={marginBox} overflowX="auto">
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Year</Th>
-            <Th>Pick Record</Th>
-            <Th>Winner</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data?.histories?.map((his) => {
-            const user = his.user;
-            const avatarUrl = user?.avatarImage?.image?.publicUrlTransformed;
-
-            return (
-              <Tr key={his.id}>
-                <Td>{his.year}</Td>
-                <Td>{his.display}</Td>
-                <Td>
-                  <HStack>
-                    <Avatar size="sm" name={user?.userName || ''} src={avatarUrl || ''} />
-                    <Text>{user?.userName}</Text>
-                  </HStack>
-                </Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </Box>
-  );
+  return <div>nope</div>;
 }
---
+++ components/contests/HistoryTable.tsx
@@ -0,0 +1,69 @@
+import {
+  Center,
+  Spinner,
+  Text,
+  Box,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+  HStack,
+  Avatar,
+} from '@chakra-ui/react';
+import { useHistoriesByTypeQuery, HistoryContestTypeType } from '../../generated/graphql-types';
+
+type HistoryTableProps = {
+  contestType: HistoryContestTypeType;
+};
+
+export default function HistoryTable({ contestType }: HistoryTableProps): JSX.Element {
+  const { data, loading } = useHistoriesByTypeQuery({
+    variables: { contestType },
+  });
+
+  const marginBox = useBreakpointValue({ base: 1, sm: 2, md: 6 });
+
+  if (loading) {
+    return (
+      <Center marginTop={'30vh'}>
+        <Spinner color="red.500" marginLeft="auto" marginRight="auto" size="xl" />
+      </Center>
+    );
+  }
+
+  return (
+    <Box borderWidth="1px" borderRadius="lg" padding={marginBox} m={marginBox} overflowX="auto">
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Year</Th>
+            <Th>Pick Record</Th>
+            <Th>Winner</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data?.histories?.map((his) => {
+            const user = his.user;
+            const avatarUrl = user?.avatarImage?.image?.publicUrlTransformed;
+
+            return (
+              <Tr key={his.id}>
+                <Td>{his.year}</Td>
+                <Td>{his.display}</Td>
+                <Td>
+                  <HStack>
+                    <Avatar size="sm" name={user?.userName || ''} src={avatarUrl || ''} />
+                    <Text>{user?.userName}</Text>
+                  </HStack>
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+}
