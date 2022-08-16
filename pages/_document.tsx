4c61a526da5236f758fa4b55ebde3766930afb9d
Brock Tillotson <brock.m.tillotson@gmail.com>
2022-08-15 19:36:50 -0700

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,8 @@ export default class MyDocument extends Document {
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📈</text></svg>"
           />
           <title>BTBets</title>
+          <meta property="og:title" content="BT Bets" />
+          <meta property="og:image" content="https://www.btbets.ml/images/bt-bets-logo.png" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
