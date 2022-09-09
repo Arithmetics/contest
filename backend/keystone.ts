045a7ee28c504523923da0f38dcca89c32dbe3cf
Brock Tillotson <brock.m.tillotson@gmail.com>
2022-09-09 09:32:06 -0700

    add job back in

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,7 +6,7 @@ import 'dotenv/config';
 
 import { sendPasswordResetEmail } from './lib/mail';
 // import { insertSeedData } from './seedData';
-// import { startDailyStandingsJob } from './standingsJob';
+import { startDailyStandingsJob } from './standingsJob';
 
 import { User } from './schemas/User';
 import { Contest } from './schemas/Contest';
@@ -64,19 +64,19 @@ export default auth.withAuth(
       provider: 'postgresql',
       url: `${process.env.DATABASE_URL}?pool_timeout=0` || 'postgres://localhost:5432/contest',
       useMigrations: true,
-      // async onConnect(context) {
-      async onConnect() {
+      async onConnect(context) {
+        // async onConnect() {
         console.log('connected');
 
         cron.schedule('0 0 14 * * *', () => {
-          // console.log('running NFL standing job!');
+          console.log('running NFL standing job!');
 
-          // startDailyStandingsJob(
-          //   context,
-          //   'ckre48xe10960292pu1w1puj8',
-          //   17,
-          //   'http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams?limit=100'
-          // );
+          startDailyStandingsJob(
+            context,
+            'cl6sco44c19150jmcou8s8rlg',
+            17,
+            'http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams?limit=100'
+          );
           // console.log('running NBA standing job!');
           // startDailyStandingsJob(
           //   context,
@@ -84,7 +84,7 @@ export default auth.withAuth(
           //   82,
           //   'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams?limit=100'
           // );
-          console.log('NO CRON JOBS SCHEDULED');
+          // console.log('NO CRON JOBS SCHEDULED');
         });
 
         if (process.argv.includes('--seed-data')) {
