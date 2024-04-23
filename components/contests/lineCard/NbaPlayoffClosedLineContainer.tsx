37f2dbb948dd9728bc80ad7fb822dfdd13f60fa4
Arithmetics <brock.m.tillotson@gmail.com>
2024-04-23 15:29:44 -0700

    visuals

--- components/contests/lineCard/NbaPlayoffClosedLineContainer.tsx
+++ components/contests/lineCard/NbaPlayoffClosedLineContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, Tooltip } from '@chakra-ui/react';
+import { Box, Tooltip, useBreakpointValue } from '@chakra-ui/react';
 
 type NbaPlayoffClosedLineContainerProps = {
   userHasBet: boolean;
@@ -11,11 +11,14 @@ export function NbaPlayoffClosedLineContainer({
   userIsLoggedIn,
   children,
 }: NbaPlayoffClosedLineContainerProps): JSX.Element {
+  const maxW = useBreakpointValue({ base: '100%', md: '200px' });
+  const width = useBreakpointValue({ base: '80%', md: 'auto' });
+
   return (
     <Tooltip label={!userIsLoggedIn && 'Log in to bet'}>
       <Box
-        maxW="200px"
-        // width="full"
+        maxW={maxW}
+        width={width}
         bg={'gray.600'}
         border={userHasBet ? '1px' : ''}
         borderColor={userHasBet ? 'teal.500' : ''}
