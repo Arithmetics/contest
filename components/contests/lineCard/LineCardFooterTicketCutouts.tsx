eed36aceb0de9f16404bcd95f8847a916ef9778f
Arithmetics <brock.m.tillotson@gmail.com>
2025-01-09 20:18:53 -0800

    color fix

--- components/contests/lineCard/LineCardFooterTicketCutouts.tsx
+++ components/contests/lineCard/LineCardFooterTicketCutouts.tsx
@@ -11,9 +11,9 @@ export const LineCardFooterTicketCutouts = ({ useBorder }: { useBorder: boolean 
         top: '-6px',
         backgroundColor: '#1a202c',
         borderRadius: '0px 6px 6px 0px',
-        borderTop: useBorder ? '1px #319795 solid' : 'none',
-        borderRight: useBorder ? '1px #319795 solid' : 'none',
-        borderBottom: useBorder ? '1px #319795 solid' : 'none',
+        borderTop: useBorder ? '1px #3302ff solid' : 'none',
+        borderRight: useBorder ? '1px #3302ff solid' : 'none',
+        borderBottom: useBorder ? '1px #3302ff solid' : 'none',
       }}
     />
     <div
@@ -25,9 +25,9 @@ export const LineCardFooterTicketCutouts = ({ useBorder }: { useBorder: boolean 
         top: '-6px',
         backgroundColor: '#1a202c',
         borderRadius: '6px 0px 0px 6px',
-        borderTop: useBorder ? '1px #319795 solid' : 'none',
-        borderLeft: useBorder ? '1px #319795 solid' : 'none',
-        borderBottom: useBorder ? '1px #319795 solid' : 'none',
+        borderTop: useBorder ? '1px #3302ff solid' : 'none',
+        borderLeft: useBorder ? '1px #3302ff solid' : 'none',
+        borderBottom: useBorder ? '1px #3302ff solid' : 'none',
       }}
     />
   </>
